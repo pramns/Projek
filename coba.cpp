2293777a3fe4e387b8868ee27c45c000b97694c8
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-02-23 15:39:25 +0700

    menambah variabel numerik

--- coba.cpp
+++ coba.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main() {
     string nama;
     char kelas;
+    int umur;
+    float TB;
    cout << "kamu adalah = ";
    cin >> nama;
-   
-   cout <<"kamu berada di kelas = ";
+   cout << "kamu berada di kelas = ";
    cin >> kelas;
+   
    
 }
